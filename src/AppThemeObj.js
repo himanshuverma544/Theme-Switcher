5fdce302ea4cdc6fe41ef369ce478030d67d797b
Himanshu Verma <himanshuverma544@gmail.com>
2023-02-16 23:42:22 +0530

    add: created object to switch theme values between light and dark

---
+++ src/AppThemeObj.js
@@ -0,0 +1,42 @@
+const AppThemeObj = { 
+  
+  light: {
+    body: {
+      backgroundColor: "#FFF"
+    },
+    heading: {
+      textColor: "#000",
+      textContent: "Light"
+    },
+    img: {
+      name: "light-theme.gif",
+      alt: "Sun Image"
+    },
+    btn: {
+      backgroundColor: "#555",
+      textColor: "#FFF",
+      textContent: "Dark",
+    }
+  },
+
+  dark: {
+    body: {
+      backgroundColor: "#333"
+    },
+    heading: {
+      textColor: "#FFF",
+      textContent: "Dark"
+    },
+    img: {
+      name: "dark-theme.gif",
+      alt: "Moon Image"
+    },
+    btn: {
+      backgroundColor: "#e7e7e7",
+      textColor: "#000",
+      textContent: "Light",
+    }
+  }
+};
+
+export default AppThemeObj;
