a3e50a68e40da2c9f8cafb1a07ca8b2ba9fc148d
Himanshu Verma <himanshuverma544@gmail.com>
2023-03-12 16:30:30 +0530

    update: memoized the getImage function

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,26 @@
-export function getImage(name) 
+export const memoGetImage = memoFunc(getImage);
+
+function getImage(name) 
 {
   let image = require(`./assets/images/${name}`);
   return image;
 }
+
+function memoFunc(func)
+{
+  var cache = {};
+  
+  return function (...args) {
+    const key = args;
+
+    if (key in cache) {
+      return cache[key];
+    }
+    else {
+      const result = func(...args);
+      cache[key] = result;
+      return result;
+    }
+  }
+}
+
